d938633dd3a61e612c789b9a92e9b7a4bfc8a841
MarcHarony <nathan.vico97@gmail.com>
2018-02-08 12:07:10 +0100

    fin de seance

--- test_utils.py
+++ test_utils.py
@@ -7,26 +7,37 @@ import utils
 
 class TestUtils(unittest.TestCase):
     def test_fact(self):
+        """
         a = fact(1)
         if a == 1 :
             print("fact(1) ok")
         else :
             print("fact(1) failure")
-    
+        """
+        self_compute(self):
+            self.assertEqual(utils.compute(1),1)
+            self.assertEqual(utils.compute(5),120)
     def test_roots(self):
+        """
         a = roots(1,-2,1)
         if a == 1 :
             print("one root ok")
         else :
             print("one root failure")
-    
+        """
+        self_compute(self):
+            self.assertEqual(utils.compute(1,-2,1),1)
+        
     def test_integrate(self):
-        a = integrate(x,1,2)
+        """
+        a = integrate('x',1,2)
         if a == 1.5 :
             print("integration ok")
         else :
             print("integration failure")
-        pass
+        """
+        self_compute(self):
+            self.assertEqual(utils.compute('machin', 0,3),11)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
-# utils.py
+    # utils.py
 # Math library
 # Author: Sébastien Combéfis
-# Version: February 8, 2018
-
+# Version: February 8, 2
+from math import sqrt
 def fact(n):
     """Computes the factorial of a natural number.
     
@@ -10,15 +10,30 @@ def fact(n):
     Post: Returns the factorial of 'n'.
     Throws: ValueError if n < 0
     """
+    a = n
+    facto = 1
+    while a > 0 :
+        facto *= a
+        a-=1
+    return facto
 
 def roots(a, b, c):
-    """Computes the roots of the ax^2 + bx + x = 0 polynomial.
+    """Computes the roots of the ax^2 + bx + c = 0 polynomial.
     
     Pre: -
     Post: Returns a tuple with zero, one or two elements corresponding
           to the roots of the ax^2 + bx + c polynomial.
     """
-    pass
+    rho = b**2-4*a*c
+    if rho >0 :
+        root_1 = (-b+ sqtr(rho))/(2*a)
+        root_2 = (-b- sqtr(rho))/(2*a)
+        return root_1 ,root_2
+    elif rho == 0 :
+        root = -b/(2*a)
+        return (root)
+    else :
+        return ()
 
 def integrate(function, lower, upper):
     """Approximates the integral of a fonction between two bounds
@@ -29,9 +44,14 @@ def integrate(function, lower, upper):
     Post: Returns an approximation of the integral from 'lower' to 'upper'
           of the specified 'function'.
     """
-    pass
+    coconut = 0
+    a = lower
+    while a < upper :
+        coconut += -1+(a+1)**2 #pas le temps de créer une fonction qui interprète un string
+        a+=1
+    return coconut
 
 if __name__ == '__main__':
     print(fact(5))
     print(roots(1, 0, 1))
-    print(integrate('x ** 2 - 1', -1, 1))
+    print(integrate('x ** 2 - 1', 0, 3))
